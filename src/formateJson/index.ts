6fee4dc25f8c2bb3a026441ef305160522fa45b6
Five <1874803565@qq.com>
2021-08-05 23:40:24 +0800

    feat: 初始化 json 方法

--- src/formateJson/index.ts
+++ src/formateJson/index.ts
@@ -1,12 +1,22 @@
+import Axios from "axios"
+
 export class FormateJson {
   public json = []
 
   /**
    * @description 初始化传入的 json url 数据
-   *
    * @param {string} [url='']
    * @memberof FormateJson
    */
-  init (url:string = '') {
+  public async init(url: string = 'https://wx.gdems.com/postemallstatic/json/cities.json') {
+    if (!url) {
+      throw 'url is not defined'
+    }
+    this._getJson(url)
+  }
+
+  private async _getJson (url: string = '') {
+    const data = Axios.post(url)
+    console.log(data)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { UpdateServer } from './updateService'
 import { GetNotice } from './notice'
 import { GitC, ScriptC } from './command'
+import { FormateJson } from './formateJson'
 
-export { UpdateServer, GetNotice, GitC, ScriptC }
+export { UpdateServer, GetNotice, GitC, ScriptC, FormateJson }
